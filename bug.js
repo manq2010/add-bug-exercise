3471cf364876a08cd9f97c25ce353b0823eff3e1
Mancoba Sihlongonyane <manq2010@gmail.com>
2022-10-14 14:32:31 +0200

    Add javascript file

---
+++ bug.js
@@ -0,0 +1,11 @@
+const function(stringValue, stringLength) {
+    let result = ''
+    for (let i = 0; i < stringLength; i++ ){
+        result = stringValue
+    }
+
+    if (stringLength < 0 < 1){
+        return 'ERROR'
+    }
+return result
+};
